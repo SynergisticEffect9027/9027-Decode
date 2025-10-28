fb54d2f5702b6bfa402a94690443a2bd1303fb78
Auralia <auraliaibel@gmail.com>
2025-10-28 10:08:27 -0500

    Added the test class for both passthrough and turret together

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/IntakePassthrough.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/IntakePassthrough.java
@@ -6,17 +6,20 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Continuous Servo TEST")
-public class ContinuousServoTEST extends LinearOpMode {
+@TeleOp(name = "Passthrough+Launcher TEST")
+public class IntakePassthrough extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor intakeMotor = null;
     private CRServo testServo;
 
     @Override
     public void runOpMode() {
         testServo = hardwareMap.get(CRServo.class, "testServo");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
 
         testServo.resetDeviceConfigurationForOpMode();
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
 
 
         // Wait for the game to start (driver presses START)
@@ -36,7 +39,14 @@ public class ContinuousServoTEST extends LinearOpMode {
             } else {
                 testServo.setPower(0.0);
             }
-
+            if (gamepad1.x) {
+                intakeMotor.setPower(1.0);
+            } else if (gamepad1.y) {
+                intakeMotor.setPower(-1.0);
+            } else {
+                intakeMotor.setPower(0.0);
             }
+
         }
     }
+}
